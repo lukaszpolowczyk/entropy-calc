e3f9456359fc704a01e24aa6316cd06f6386447e
Łukasz Polowczyk <dev.lukaszpolowczyk@gmail.com>
2016-08-28 15:33:46 +0200

    second beta

--- scripts/main/szpion.js
+++ scripts/main/szpion.js
@@ -13,114 +13,151 @@ define(["libs/libs", "main/question"], function(libs, Question) {
 		containerEl.appendChild(this.szpionEl);
 		
 		this.resultEl = this.szpionEl.querySelector(".result");
-		
-		var questionC = new Question(this);
-		this.commonQuestionsObs.push(questionC);
-		this.commonQuestionsEl.appendChild(questionC.questionEl);
-		
-		var questionF = new Question(this);
-		this.foreignQuestionsObs.push(questionF);
-		this.foreignQuestionsEl.appendChild(questionF.questionEl);
+//		
+//		var questionC = new Question(this);
+//		this.commonQuestionsObs.push(questionC);
+//		this.commonQuestionsEl.appendChild(questionC.questionEl);
+//		
+//		var questionF = new Question(this);
+//		this.foreignQuestionsObs.push(questionF);
+//		this.foreignQuestionsEl.appendChild(questionF.questionEl);
 		
 		this.addCommonQuestionButtonEl = this.szpionEl.querySelector(".add-common-question-button");
 		this.addForeignQuestionButtonEl = this.szpionEl.querySelector(".add-foreigns-question-button");
 		
-		this.addCommonQuestionButtonEl.addEventListener("click", ()=> {
+		var leC = ()=> {
 			var questionC = new Question(this);
 			this.commonQuestionsObs.push(questionC);
 			this.commonQuestionsEl.appendChild(questionC.questionEl);
-		});
-		
-		this.addForeignQuestionButtonEl.addEventListener("click", ()=> {
+		};
+		var leF = ()=> {
 			var questionF = new Question(this);
 			this.foreignQuestionsObs.push(questionF);
 			this.foreignQuestionsEl.appendChild(questionF.questionEl);
-		});
+		};
 		
-		// jeszcze funkcja automatycznego dodawania kolejnego pustego
-		// jeszcze kolor zaznaczonej odpowiedzi (atrybut .question[data-false-answer] itd)
+		leC();
+		leF();
+		
+		this.addCommonQuestionButtonEl.addEventListener("click", leC);
+		this.addForeignQuestionButtonEl.addEventListener("click", leF);
+		
 		// wpisanie warości prawdopodobieństwa do każdego
 		// mozliwoś zmieny prawdopodobieństw ręcznie
 	}
 	
-	Szpion.prototype.count = function () {
-		this.commonTrueAnswersObs = this.commonQuestionsObs.filter((commonQuestion)=> {
-			return commonQuestion.answer === true;
-		});
-		this.commonFalseAnswersObs = this.commonQuestionsObs.filter((commonQuestion)=> {
-			return commonQuestion.answer === false;
-		});
-		this.commonNoAnswersObs = this.commonQuestionsObs.filter((commonQuestion)=> {
-			return commonQuestion.answer === "";
-		});
-		
-		this.commonTrueAnswersObsLength = this.commonTrueAnswersObs.length;
-		this.commonFalseAnswersObsLength = this.commonFalseAnswersObs.length;
-		this.commonNoAnswersObsLength = this.commonNoAnswersObs.length;
-		
-		var commonAnswerVal = 1 / (this.commonTrueAnswersObsLength + this.commonFalseAnswersObsLength);
-		
-		var commonAnswersVal = commonAnswerVal * this.commonTrueAnswersObsLength;
-		
-		this.foreignTrueAnswersObs = this.foreignQuestionsObs.filter((foreignQuestion)=> {
-			return foreignQuestion.answer === true;
-		});
-		this.foreignFalseAnswersObs = this.foreignQuestionsObs.filter((foreignQuestion)=> {
-			return foreignQuestion.answer === false;
-		});
-		this.foreignNoAnswersObs = this.foreignQuestionsObs.filter((foreignQuestion)=> {
-			return foreignQuestion.answer === "";
-		});
-		
-		this.foreignTrueAnswersObsLength = this.foreignTrueAnswersObs.length;
-		this.foreignFalseAnswersObsLength = this.foreignFalseAnswersObs.length;
-		this.foreignNoAnswersObsLength = this.foreignNoAnswersObs.length;
-		
-		var foreignAnswerVal = 1 / (this.foreignTrueAnswersObsLength + this.foreignFalseAnswersObsLength);
+//	Szpion.prototype.questionsSideCount = function () {
+//		this.commonTrueAnswersObs = this.commonQuestionsObs.filter((commonQuestion)=> {
+//			return commonQuestion.answer === true;
+//		});
+//		this.commonFalseAnswersObs = this.commonQuestionsObs.filter((commonQuestion)=> {
+//			return commonQuestion.answer === false;
+//		});
+//		this.commonNoAnswersObs = this.commonQuestionsObs.filter((commonQuestion)=> {
+//			return commonQuestion.answer === "";
+//		});
+//
+//		this.commonTrueAnswersObsLength = this.commonTrueAnswersObs.length;
+//		this.commonFalseAnswersObsLength = this.commonFalseAnswersObs.length;
+//		this.commonNoAnswersObsLength = this.commonNoAnswersObs.length;
+//
+//		var commonAnswerVal = 1 / (this.commonTrueAnswersObsLength + this.commonFalseAnswersObsLength);
+//
+//		var commonAnswersVal = commonAnswerVal * this.commonTrueAnswersObsLength;
+//	};
 
-		var foreignAnswersVal = foreignAnswerVal * this.foreignTrueAnswersObsLength;
+//	this.foreignTrueAnswersObs = this.foreignQuestionsObs.filter((foreignQuestion)=> {
+//		return foreignQuestion.answer === true;
+//	});
+//	this.foreignFalseAnswersObs = this.foreignQuestionsObs.filter((foreignQuestion)=> {
+//		return foreignQuestion.answer === false;
+//	});
+//	this.foreignNoAnswersObs = this.foreignQuestionsObs.filter((foreignQuestion)=> {
+//		return foreignQuestion.answer === "";
+//	});
+//
+//	this.foreignTrueAnswersObsLength = this.foreignTrueAnswersObs.length;
+//	this.foreignFalseAnswersObsLength = this.foreignFalseAnswersObs.length;
+//	this.foreignNoAnswersObsLength = this.foreignNoAnswersObs.length;
+//
+//	var foreignAnswerVal = 1 / (this.foreignTrueAnswersObsLength + this.foreignFalseAnswersObsLength);
+//
+//	var foreignAnswersVal = foreignAnswerVal * this.foreignTrueAnswersObsLength;
+
+	var questionsSideCount = function (questionsObs, sideAnswerImportance) {
+		var side = {};
+		side.trueAnswersObs = questionsObs.filter((question)=> {
+			return question.answer === true;
+		});
+		side.falseAnswersObs = questionsObs.filter((question)=> {
+			return question.answer === false;
+		});
+		side.noAnswersObs = questionsObs.filter((question)=> {
+			return question.answer === "";
+		});
+
+		side.trueAnswersObsLength = side.trueAnswersObs.length;
+		side.falseAnswersObsLength = side.falseAnswersObs.length;
+		side.noAnswersObsLength = side.noAnswersObs.length;
+
+		side.answerVal = 1 / (side.trueAnswersObsLength + side.falseAnswersObsLength);
+
+		side.answersVal = side.answerVal * side[sideAnswerImportance];
+
+		side.numbers = `0`;
+
+		if (side[sideAnswerImportance] > 0) {
+			side.numbers = `${libs.round(side.answerVal, 3)} + `.repeat(side[sideAnswerImportance]-1);
+			side.numbers += libs.round(side.answerVal, 3);
+		}
+		
+//		for (var i = 1; i < side.trueAnswersObsLength; i++) {
+		//	side.numbers += `${libs.round(side.answerVal, 3)} + `;
+//		}
+		
+		return side;
+	};
+	
+	Szpion.prototype.count = function () {
+		var common = questionsSideCount(this.commonQuestionsObs, "trueAnswersObsLength");
+		var foreign = questionsSideCount(this.foreignQuestionsObs, "falseAnswersObsLength");
+		
+
+//		common.answersVal = common.answerVal * common.trueAnswersObsLength;
+//
+//		common.numbers = `0`;
+//
+//		if (common.trueAnswersObsLength > 0) {
+//			common.numbers = `${libs.round(common.answerVal, 3)} + `.repeat(common.trueAnswersObsLength-1);
+//			common.numbers += libs.round(common.answerVal, 3);
+//		}
+//		
+//		
+//		foreign.answersVal = foreign.answerVal * foreign.falseAnswersObsLength;
+//
+//		foreign.numbers = `0`;
+//
+//		if (foreign.trueAnswersObsLength > 0) {
+//			foreign.numbers = `${libs.round(foreign.answerVal, 3)} + `.repeat(foreign.trueAnswersObsLength-1);
+//			foreign.numbers += libs.round(foreign.answerVal, 3);
+//		}
 		
 		
 		// result
-		var preResult = commonAnswersVal * foreignAnswersVal;
+		var preResult = common.answersVal * foreign.answersVal;
 		var result = 1 - preResult;
 		
 
-		var commonNumbers = ``;
-		for (var i = 1; i < this.commonTrueAnswersObsLength; i++) {
-			commonNumbers += `${libs.round(commonAnswerVal, 3)} + `;
-		}
-		commonNumbers += libs.round(commonAnswerVal, 3);
-		var foreignNumbers = ``;
-		for (var ix = 1; ix < this.foreignTrueAnswersObsLength; ix++) {
-			foreignNumbers += `${libs.round(foreignAnswerVal, 3)} + `;
-		}
-		foreignNumbers += libs.round(foreignAnswerVal, 3);
-
-		var numbersAll = `(${commonNumbers}) * (${foreignNumbers}) = ${libs.round(commonAnswersVal, 3)} * ${libs.round(foreignAnswersVal, 3)} = ${libs.round(preResult, 3)} <br/> 1 - ${libs.round(preResult, 3)} = ${libs.round(result, 3)}`;
-
+		var numbersAll = `(${common.numbers}) * (${foreign.numbers}) = ${libs.round(common.answersVal, 3)} * ${libs.round(foreign.answersVal, 3)} = ${libs.round(preResult, 3)} <br/> 1 - ${libs.round(preResult, 3)} = ${libs.round(result, 3)}`;
 		
-		
-		if (result > .5 && result <= 1) {
+		if (result > 0.5 && result <= 1) {
 			this.resultEl.innerHTML = `Wynik: Szpion na ${libs.round(result*100, 3)}%. <br/>${numbersAll}`;
 		} else
-		if (result >= 0 && result <= .5) {
+		if (result >= 0 && result <= 0.5) {
 			this.resultEl.innerHTML = `Wynik: Szpion na ${libs.round(result*100, 3)}%. <br/>${numbersAll}`;
 		} else {
-			this.resultEl.innerHTML = "Wynik: Brakujace dane lub jakiś błąd w programie.";
+			this.resultEl.innerHTML = "Wynik: Brakujące dane lub jakiś błąd w programie. Potrzeba co najmniej jednej odpowiedzi o współny interes i conajmniej jednej o obcy interes.";
 		}
-		
-//		if (this.commonNoAnswersObsLength === 0) {
-//			var questionC = new Question(this);
-//			this.commonQuestionsObs.push(questionC);
-//			this.commonQuestionsEl.appendChild(questionC.questionEl);
-//		}
-//
-//		if (this.foreignNoAnswersObsLength === 0) {
-//			var questionF = new Question(this);
-//			this.foreignQuestionsObs.push(questionF);
-//			this.foreignQuestionsEl.appendChild(questionF.questionEl);
-//		}
 	};
 	
 	// zrobić klasę z questionsBlock żeby się nie duplikować tak bardzo
