c6808af4e7d44651c81030b83c95fa2742a2a45b
Łukasz Polowczyk <dev.lukaszpolowczyk@gmail.com>
2016-08-30 00:14:45 +0200

    play first beta

--- scripts/main/szpion.js
+++ scripts/main/szpion.js
@@ -13,14 +13,6 @@ define(["libs/libs", "main/question"], function(libs, Question) {
 		containerEl.appendChild(this.szpionEl);
 		
 		this.resultEl = this.szpionEl.querySelector(".result");
-//		
-//		var questionC = new Question(this);
-//		this.commonQuestionsObs.push(questionC);
-//		this.commonQuestionsEl.appendChild(questionC.questionEl);
-//		
-//		var questionF = new Question(this);
-//		this.foreignQuestionsObs.push(questionF);
-//		this.foreignQuestionsEl.appendChild(questionF.questionEl);
 		
 		this.addCommonQuestionButtonEl = this.szpionEl.querySelector(".add-common-question-button");
 		this.addForeignQuestionButtonEl = this.szpionEl.querySelector(".add-foreigns-question-button");
@@ -44,47 +36,11 @@ define(["libs/libs", "main/question"], function(libs, Question) {
 		
 		// wpisanie warości prawdopodobieństwa do każdego
 		// mozliwoś zmieny prawdopodobieństw ręcznie
+		
+		this.count();
 	}
 	
-//	Szpion.prototype.questionsSideCount = function () {
-//		this.commonTrueAnswersObs = this.commonQuestionsObs.filter((commonQuestion)=> {
-//			return commonQuestion.answer === true;
-//		});
-//		this.commonFalseAnswersObs = this.commonQuestionsObs.filter((commonQuestion)=> {
-//			return commonQuestion.answer === false;
-//		});
-//		this.commonNoAnswersObs = this.commonQuestionsObs.filter((commonQuestion)=> {
-//			return commonQuestion.answer === "";
-//		});
-//
-//		this.commonTrueAnswersObsLength = this.commonTrueAnswersObs.length;
-//		this.commonFalseAnswersObsLength = this.commonFalseAnswersObs.length;
-//		this.commonNoAnswersObsLength = this.commonNoAnswersObs.length;
-//
-//		var commonAnswerVal = 1 / (this.commonTrueAnswersObsLength + this.commonFalseAnswersObsLength);
-//
-//		var commonAnswersVal = commonAnswerVal * this.commonTrueAnswersObsLength;
-//	};
-
-//	this.foreignTrueAnswersObs = this.foreignQuestionsObs.filter((foreignQuestion)=> {
-//		return foreignQuestion.answer === true;
-//	});
-//	this.foreignFalseAnswersObs = this.foreignQuestionsObs.filter((foreignQuestion)=> {
-//		return foreignQuestion.answer === false;
-//	});
-//	this.foreignNoAnswersObs = this.foreignQuestionsObs.filter((foreignQuestion)=> {
-//		return foreignQuestion.answer === "";
-//	});
-//
-//	this.foreignTrueAnswersObsLength = this.foreignTrueAnswersObs.length;
-//	this.foreignFalseAnswersObsLength = this.foreignFalseAnswersObs.length;
-//	this.foreignNoAnswersObsLength = this.foreignNoAnswersObs.length;
-//
-//	var foreignAnswerVal = 1 / (this.foreignTrueAnswersObsLength + this.foreignFalseAnswersObsLength);
-//
-//	var foreignAnswersVal = foreignAnswerVal * this.foreignTrueAnswersObsLength;
-
-	var questionsSideCount = function (questionsObs, sideAnswerImportance) {
+	var questionsSideCount = function (questionsObs) {
 		var side = {};
 		side.trueAnswersObs = questionsObs.filter((question)=> {
 			return question.answer === true;
@@ -98,71 +54,70 @@ define(["libs/libs", "main/question"], function(libs, Question) {
 
 		side.trueAnswersObsLength = side.trueAnswersObs.length;
 		side.falseAnswersObsLength = side.falseAnswersObs.length;
+		side.answersObsLength = side.trueAnswersObsLength + side.falseAnswersObsLength;
 		side.noAnswersObsLength = side.noAnswersObs.length;
 
-		side.answerVal = 1 / (side.trueAnswersObsLength + side.falseAnswersObsLength);
 
-		side.answersVal = side.answerVal * side[sideAnswerImportance];
-
-		side.numbers = `0`;
-
-		if (side[sideAnswerImportance] > 0) {
-			side.numbers = `${libs.round(side.answerVal, 3)} + `.repeat(side[sideAnswerImportance]-1);
-			side.numbers += libs.round(side.answerVal, 3);
-		}
-		
-//		for (var i = 1; i < side.trueAnswersObsLength; i++) {
-		//	side.numbers += `${libs.round(side.answerVal, 3)} + `;
-//		}
-		
 		return side;
 	};
 	
 	Szpion.prototype.count = function () {
-		var common = questionsSideCount(this.commonQuestionsObs, "trueAnswersObsLength");
-		var foreign = questionsSideCount(this.foreignQuestionsObs, "falseAnswersObsLength");
+		var common = questionsSideCount(this.commonQuestionsObs);
+		var foreign = questionsSideCount(this.foreignQuestionsObs);
 		
-
-//		common.answersVal = common.answerVal * common.trueAnswersObsLength;
-//
-//		common.numbers = `0`;
-//
-//		if (common.trueAnswersObsLength > 0) {
-//			common.numbers = `${libs.round(common.answerVal, 3)} + `.repeat(common.trueAnswersObsLength-1);
-//			common.numbers += libs.round(common.answerVal, 3);
-//		}
+//		var allAnswersObsLength = common.answersObsLength + foreign.answersObsLength;
 //		
+//		var weightSingleAnswer = 1 / allAnswersObsLength;
 //		
-//		foreign.answersVal = foreign.answerVal * foreign.falseAnswersObsLength;
-//
-//		foreign.numbers = `0`;
-//
-//		if (foreign.trueAnswersObsLength > 0) {
-//			foreign.numbers = `${libs.round(foreign.answerVal, 3)} + `.repeat(foreign.trueAnswersObsLength-1);
-//			foreign.numbers += libs.round(foreign.answerVal, 3);
+//		var positiveAnswers = common.trueAnswersObsLength + foreign.falseAnswersObsLength;
+//		var negativeAnswers = common.falseAnswersObsLength + foreign.trueAnswersObsLength;
+//		var noSzpion = weightSingleAnswer * positiveAnswers;
+//		var szpion = 1 - noSzpion;
+		
+//		common.weightSingleAnswer = 1 / common.answersObsLength;
+//		foreign.weightSingleAnswer = 1 / foreign.answersObsLength;
+//		
+//		common.val = common.weightSingleAnswer * common.trueAnswersObsLength;
+//		foreign.val = foreign.weightSingleAnswer * foreign.trueAnswersObsLength;
+//		if (foreign.trueAnswersObsLength === 0) {
+//			foreign.val = 1;
 //		}
 		
+//		var noSzpion = common.val * foreign.val;
+//		var szpion = 1 - noSzpion;
 		
-		// result
-		var preResult = common.answersVal * foreign.answersVal;
-		var result = 1 - preResult;
+		var allTrueAnswersObsLength = common.trueAnswersObsLength + foreign.trueAnswersObsLength;
+		var weightOneAnswer = 1 / allTrueAnswersObsLength;
+		common.weightTrueAnswers = weightOneAnswer * common.trueAnswersObsLength;
+		foreign.weightTrueAnswers = weightOneAnswer * foreign.trueAnswersObsLength;
 		
+		common.weight = common.weightTrueAnswers / (common.trueAnswersObsLength + common.falseAnswersObsLength);
+		common.endWeight = common.weightTrueAnswers - (common.weight * common.falseAnswersObsLength);
 
-		var numbersAll = `(${common.numbers}) * (${foreign.numbers}) = ${libs.round(common.answersVal, 3)} * ${libs.round(foreign.answersVal, 3)} = ${libs.round(preResult, 3)} <br/> 1 - ${libs.round(preResult, 3)} = ${libs.round(result, 3)}`;
+		foreign.weight = foreign.weightTrueAnswers / (foreign.trueAnswersObsLength + foreign.falseAnswersObsLength);
+		foreign.endWeight = foreign.weightTrueAnswers - (foreign.weight * foreign.falseAnswersObsLength);
 		
-		if (result > 0.5 && result <= 1) {
-			this.resultEl.innerHTML = `Wynik: Szpion na ${libs.round(result*100, 3)}%. <br/>${numbersAll}`;
+		var noSzpion = common.endWeight/(common.endWeight + foreign.endWeight);
+		var szpion = 1 - noSzpion;
+		
+		if (common.answersObsLength === 0 && foreign.answersObsLength === 0) {
+			this.resultEl.innerHTML = `Wynik: Brak odpowiedzi. Brak informacji. Maksymalna entropia. 50% szansy na "bycie agentem" i 50% szansy na "nie bycie agentem".`;
 		} else
-		if (result >= 0 && result <= 0.5) {
-			this.resultEl.innerHTML = `Wynik: Szpion na ${libs.round(result*100, 3)}%. <br/>${numbersAll}`;
+		if (common.answersObsLength === 0 || foreign.answersObsLength === 0) {
+			this.resultEl.innerHTML = "Brak wyniku: Potrzeba co najmniej po jednej odpowiedzi.";
+		} else
+			if (common.trueAnswersObsLength === 0 && foreign.trueAnswersObsLength === 0) {
+				this.resultEl.innerHTML = `Wynik: 50% szansy na "bycie agentem" i 50% szansy na nie "bycie agentem"`;
+			} else 
+		if (szpion >= 0 && szpion <= 1 && noSzpion >= 0 && noSzpion <= 1) {
+			this.resultEl.innerHTML = `Wynik: ${libs.round(szpion*100, 1)}% szansy na "bycie agentem" i ${libs.round(noSzpion*100, 1)}% szansy na "nie bycie agentem".<br/>`;
 		} else {
-			this.resultEl.innerHTML = "Wynik: Brakujące dane lub jakiś błąd w programie. Potrzeba co najmniej jednej odpowiedzi o współny interes i conajmniej jednej o obcy interes.";
+			this.resultEl.innerHTML = "Brak wyniku: Jakiś błąd.";
 		}
 	};
-	
-	// zrobić klasę z questionsBlock żeby się nie duplikować tak bardzo
 	
 	return Szpion;
 	
 });
 // zrobić snippeta z this.questionEl = libs.tempEl("question"); i z this.foreignQuestionsEl = this.szpionEl.querySelector('.foreign-questions'); i z obiektem który ma to i reszte też
+// zrobić klasę z listą i dodawaniem do listy i elementu itd
